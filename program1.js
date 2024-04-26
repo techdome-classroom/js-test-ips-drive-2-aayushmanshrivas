3cc63e1d1127562fde4f0be667358f5d0ad04355
aayushmanshrivas <168168663+aayushmanshrivas@users.noreply.github.com>
2024-04-26 16:25:51 +0530

    Update program1.js
    program 1

--- program1.js
+++ program1.js
@@ -3,9 +3,24 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const bracketsMap = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in bracketsMap) {
+            if (!stack.length || stack.pop() !== bracketsMap[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
